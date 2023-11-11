900cc9c2dcb48c1749f5936e511b1ca996a40773
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-11-11 12:29:57 -0500

    add: about info & photo

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,91 +8,43 @@ function About() {
     <Container>
       <Row>
         <Col className='about-info'>
-          Originally from Upstate New York, I graduated from SUNY Oswego in 2013
-          with a dual bachelor's degree; one in Education and the other in
-          Advanced Literature. I taught in South Carolina for ten years,
-          eventually finding myself in North Carolina. <br></br>
-          <br></br>I have always had a{' '}
-          <span className='underline--magical'>
-            {' '}
-            passion for software development{' '}
-          </span>{' '}
-          and all that comes with the field - particularly reading documentation
-          into the night. 🙃 I decided to complete a six-month full stack boot
-          camp through UNC-Chapel Hill. As of August 2023, I earned my
-          certificate and I am excited to shift my career path.<br></br>
+          Hello there! Welcome to Cupcakes by Jenn, my cupcake business. My
+          name, as you may have guessed, is Jenn. I have been baking cupcakes
+          from scratch for over 12 years and started my business in 2016. I
+          often made cupcakes for work functions and was told I should open a
+          cupcake business, so thus Cupcakes by Jenn was born. I started off by
+          mainly doing small orders for birthday or graduation parties, but as
+          word spread, I have been asked to do large cupcake orders a few
+          weddings as well. So now with lots of different experiences under my
+          belt, I am looking to expand my business. I currently do have a full
+          time day job and am not looking to open a full fledged shop at this
+          point but would love to slowly grow what I do. If you are interest in
+          having Cupcakes by Jenn cupcakes available at your business for sale,
+          please reach out. And to read about my experiences with baking
+          competitions, head over to my award page. <br></br>
           <br></br>
-          My experiences have given me exceptional skills in{' '}
-          <span className='underline--magical'> leadership</span>,
-          <span className='underline--magical'> collaboration</span>, and
-          <span className='underline--magical'> problem-solving</span>. I have a
-          natural drive to organize, meet deadlines, and overcome challenges. It
-          has always been so satisfying to fix a bug or have my work make others
-          happy.
+          If you’re still reading, you might be interested in learning more
+          information about me. I grew up in Iowa and have basically only lived
+          in the Midwest. I have visited tons of places that I could see myself
+          living, but for now I still call Iowa home. When I am not working my
+          full time job or baking cupcakes - I spend time with my little herd of
+          animals. I have two French bulldogs who live with me, plus four cats.
+          Remy LeBeau and Rogue, my frenchies, are 6 years old now and are big
+          couch potatoes. My cats range from 8 years to 6 months old. Xavier is
+          8, Logan is 6.5, and my two kittens are Emma Frost and Raven Darkholme
+          who are 6 months and are siblings. Now you may see a theme in the
+          naming of my pets - and if you guessed X-Men - you were right. I am a
+          huge comic book nerd, I also nerd out on Anime too! But I will save
+          some juicy information for in case you decide to book a cooking class
+          with me. Yes, you read that right, I will soon be offering to show you
+          how Cupcakes by Jenn makes her award winning cupcakes!!!
         </Col>
         <Col>
           <img
             src={aboutPic}
-            alt='Picture of Stephanie'
+            alt='Picture of Jenn'
             className='profile-image'
           />
-        </Col>
-      </Row>
-      <Row className='about-skills'>
-        <Col>
-          <h1>My career, so far...</h1>
-          <p>
-            My time working within the field of education granted me
-            opportunities to work with a variety of stakeholders & develop a
-            strong skill set. These skills give me an upper hand when working on
-            a team & gives me the discipline to continue my own personal growth.
-            Because of this, I am consistently seeking information in order to
-            better my craft.
-          </p>
-        </Col>
-        <Col xs lg='3'>
-          <h4>
-            <ul>
-              Planning &<br></br> Organization
-            </ul>
-            <ul>Communication</ul>
-            <ul>Problem-solving</ul>
-            <ul>Adaptability</ul>
-            <ul>Patience</ul>
-          </h4>
-        </Col>
-        <Col xs lg='2'>
-          <h4>
-            <ul>Teamwork</ul>
-            <ul>Leadership</ul>
-            <ul>Creativity</ul>
-            <ul>Life-long Learner</ul>
-          </h4>
-        </Col>
-      </Row>
-      <Row className='about-certs'>
-        <Col xs lg='3'>
-          <h4>
-            <ul>
-              Full-Stack Web <br></br>Development
-            </ul>
-            <ul>
-              University of South <br></br>Carolina Coaching Teacher
-            </ul>
-            <ul>NSWF; Critical Friends Leadership</ul>
-          </h4>
-        </Col>
-        <Col>
-          <h1>My certifications</h1>
-          <p>
-            My certifications in leadership, coupled with web development, make
-            me a strong candidate in a variety of roles. I have a strong ability
-            to motivate myself & my teammates, as well as solve problems & make
-            decisions effectively. I am versed in the latest technologies &
-            tools that are present in today's industry. Being a highly adaptable
-            individual, I am able to quickly learn new skills & pivot when
-            necessary.
-          </p>
         </Col>
       </Row>
     </Container>
