0dfda47af8b22321c34c9ae802a70e9f8e2dbed4
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-11-12 15:45:35 -0500

    feat: added welcome pg banner & imgs

--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -1,5 +1,67 @@
-function Home() {
-  return <footer>Welcome</footer>;
+import { Container, Row, Col } from 'react-bootstrap';
+
+import './welcome.css';
+
+const occasions = [
+  {
+    img: 'birthdays',
+    title: 'Birthdays',
+    description: 'birthday cupcakes',
+    href: '/menu',
+  },
+  {
+    img: 'weddings',
+    title: 'Weddings',
+    description: 'wedding cupcakes',
+    href: '/menu',
+  },
+  {
+    img: 'weddings',
+    title: 'Holidays',
+    description: 'holiday cupcakes',
+    href: '/menu',
+  },
+  {
+    img: 'graduations',
+    title: 'Graduations',
+    description: 'graduation cupcakes',
+    href: '/menu',
+  },
+];
+
+function Welcome() {
+  return (
+    <Container className='welcome'>
+      <Row>
+        <Col>
+          <div className='title-container'>
+            <h2 className='title'>
+              <span className='title-word title-word-1'>Baking up </span>
+              <span className='title-word title-word-2'>a little </span>
+              <br></br>
+              <span className='title-word title-word-3'>piece of </span>
+              <span className='title-word title-word-4'>Heaven.</span>
+            </h2>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        {occasions.map((occasion, index) => (
+          <Col key={index} className='occasions'>
+            <a href={occasion.href}>
+              <div className='image-container'>
+                <img
+                  src={`https://picsum.photos/id/${index + 1}/300/300`}
+                  alt={occasion.description}
+                />
+                <div>{occasion.title}</div>
+              </div>
+            </a>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
-export default Home;
+export default Welcome;
