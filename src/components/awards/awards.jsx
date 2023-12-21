b26d4bfc9d9118aeb9db51709951f7e079ebf182
Stephanie <st3phanie.harris@gmail.com>
2023-12-21 11:18:48 -0500

    add: included info provided by client

--- src/components/awards/awards.jsx
+++ src/components/awards/awards.jsx
@@ -13,10 +13,29 @@ const award = [
   {
     img: awardImg,
     title: 'awards',
-    year: '2023',
+    year: '2019',
+    category: '2nd place	Innovative Chocolate	Spicy Mexican Hot Chocolate',
+    cupcake: '',
+    description:
+      'In 2019, Cupcakes by Jenn debuted at the fair in over 10 competitions, and that first year was a disaster! Competing taught me valuable lessons. I learned to carefully track competition schedules—8 out of 10 days in 2019—and manage the challenges of baking daily while working my day job. Juggling up to 3 competitions some days, I realized they could be overwhelming. Additionally, I discovered the importance of closely following competition instructions; unfortunately, I was disqualified from 2 in 2019. Despite these hurdles, my first year was successful—I won 5 competitions.',
+  },
+  {
+    img: awardImg,
+    title: 'awards',
+    year: '2020',
     category: '',
     cupcake: '',
-    description: 'This is where a description lives',
+    description:
+      'In 2020, the world came to a halt, including the state fair, providing an extra year for planning. The competition list is released in May, and registration is open until July 1st. Fortunately, you only need to specify the number of competitions, not their names. Entry fees cover all selected competitions. The list contains crucial details like judging dates, rules, criteria, and prizes for over 125 competitions with 600+ categories. While only a few are dessert-eligible, my creative approach helps me identify 15-20 appealing options, making the selection process more manageable.',
+  },
+  {
+    img: awardImg,
+    title: 'awards',
+    year: '2021',
+    category: '',
+    cupcake: '',
+    description:
+      'In 2021, the competition intensified as it appeared that everyone had picked up baking or cooking skills over the previous year. Despite entering 10 competitions, I only secured a placement in one, and the prize did not even cover the entry costs. It was a hit to my confidence after a successful debut year. However, I resolved not to give up easily and began planning for the next year.',
   },
   {
     img: awardImg,
@@ -29,15 +48,7 @@ const award = [
   {
     img: awardImg,
     title: 'awards',
-    year: '2021',
-    category: '',
-    cupcake: '',
-    description: 'This is where a description lives',
-  },
-  {
-    img: awardImg,
-    title: 'awards',
-    year: '2019',
+    year: '2023',
     category: '',
     cupcake: '',
     description: 'This is where a description lives',
@@ -49,13 +60,16 @@ function Award() {
     <Container>
       <Row className='award-history'>
         <h1>Jenn's Journey...</h1>
-        <h3>
-          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Ab, repudiandae! Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Accusamus corrupti eius harum ad neque
-          non cumque, provident tempore reiciendis porro repellat, mollitia,
-          voluptatem odit ipsam.
-        </h3>
+        <p>
+          My journey into the world of cupcakes began at the state fair.
+          Surrounded by the aroma of sugar and creativity, I'd wander through
+          the baking competitions, admiring the edible masterpieces and
+          thinking, "I could top that!" Years of this inspiration culminated in
+          a bold decision in 2019: to enter the competition myself. <br></br>
+          <br></br>
+          From that delicious leap, a journey of delectable delight began, one I
+          continue to love.
+        </p>
       </Row>
       <VerticalTimeline className='timeline-container'>
         {award.map((award, index) => (
@@ -67,10 +81,10 @@ function Award() {
           >
             <h1 className='vertical-timeline-element-title'>{award.year}</h1>
             <img src={award.img} />
-            <h4 className='vertical-timeline-element-subtitle'>
+            <p className='vertical-timeline-element-subtitle'>
               {award.description}
-            </h4>
-            <p>{award.category}</p>
+            </p>
+            <p>Competitions:<br></br> {award.category}</p>
             <p>{award.cupcake}</p>
           </VerticalTimelineElement>
         ))}
@@ -80,161 +94,3 @@ function Award() {
 }
 
 export default Award;
-
-// import { Container, Row, Col } from 'react-bootstrap';
-// import './awards.css';
-
-// function Awards() {
-//   const awardInfo = [
-//     {
-//       year: '2023',
-//       awards: [
-//         {
-//           title: 'Honorable Mention',
-//           items: [
-//             {
-//               title: 'Always Room for Dessert',
-//               content: 'Apple Pie',
-//             },
-//           ],
-//         },
-//         {
-//           title: 'Overall',
-//           items: [
-//             {
-//               title: 'Iowa Orchard',
-//               content: 'Apple Pie',
-//             },
-//           ],
-//         },
-//         {
-//           title: '1st',
-//           items: [
-//             {
-//               title: 'Apple Dessert',
-//               content: 'Apple Pie',
-//             },
-//             {
-//               title: 'Iowa Orchard',
-//               content: 'Apple Pie',
-//             },
-//           ],
-//         },
-//         {
-//           title: '3rd',
-//           items: [
-//             {
-//               title: 'Splenda Low to No Sugar Showdown',
-//               content: 'Key Lime Pie',
-//             },
-//             {
-//               title: 'Anything Goes, Everything Goes',
-//               content: 'Orange Creamcicle',
-//             },
-//             {
-//               title: 'Breakfast Cake',
-//               content: 'Banana Nut French Toast',
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//     {
-//       year: '2022',
-//       awards: [
-//         {
-//           title: 'Overall',
-//           items: [
-//             {
-//               title: 'Bacon, Bacon, Bacon',
-//               content: 'Chocolate Caramel Bacon',
-//             },
-//           ],
-//         },
-//         {
-//           title: '1st',
-//           items: [
-//             {
-//               title: 'Bacon, Bacon, Bacon',
-//               content: 'Chocolate Caramel Bacon',
-//             },
-//             {
-//               title: 'Breast Cancer',
-//               content: 'Strawberry Crunch',
-//             },
-//           ],
-//         },
-//         {
-//           title: '2nd',
-//           items: [
-//             {
-//               title: 'Hyvee cake',
-//               content: 'Cherry Almond',
-//             },
-//           ],
-//         },
-//         {
-//           title: '3rd',
-//           items: [
-//             {
-//               title: 'Always Room for Dessert',
-//               content: 'Apple Cider',
-//             },
-//             {
-//               title: "Iowa's Big Four",
-//               content: 'Chocolate Caramel Bacon',
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//     {
-//       year: '2021',
-//       awards: [
-//         {
-//           title: '3rd',
-//           items: [
-//             {
-//               title: 'Nostalgic',
-//               content: 'German Chocolate',
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//     {
-//       year: '2019',
-//       awards: [
-//         {
-//           title: '2nd',
-//           items: [
-//             {
-//               title: 'Innovative Chocolate',
-//               content: 'Spicy Mexican Hot Chocolate',
-//             },
-//             {
-//               title: "Iowa's Big Four",
-//               content: 'Maple Bacon Breakfast',
-//             },
-//             {
-//               title: 'Create a Winner with Gurleys',
-//               content: 'Banana Nut French Toast',
-//             },
-//           ],
-//         },
-//         {
-//           title: '3rd',
-//           items: [
-//             {
-//               title: 'Best Templeton Rye Dessert',
-//               content: 'Whiskey Smash',
-//             },
-//             {
-//               title: 'Beginners Contest',
-//               content: 'Carrot Cake',
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//   ];
