6d0c4a4e802572028c33e3522411b90823357f73
Stephanie <st3phanie.harris@gmail.com>
2023-12-27 11:46:08 -0500

    add: photos to award timeline

--- src/components/awards/awards.jsx
+++ src/components/awards/awards.jsx
@@ -7,12 +7,15 @@ import { Container, Row, Col } from 'react-bootstrap';
 import 'react-vertical-timeline-component/style.min.css';
 import './awards.css';
 
-import awardImg from '../../assets/birthday.jpg';
-import AwardGallery from '../awardGallery/awardGallery.jsx'
+import awardPic from '../../assets/awardPic.jpg';
+import cupck2019 from '../../assets/cupck2019.jpg';
+import cupck2022 from '../../assets/cupck2022.jpg';
+import cupck2023 from '../../assets/cupck2023.jpg';
+import AwardGallery from '../awardGallery/awardGallery.jsx';
 
 const award = [
   {
-    img: awardImg,
+    img: cupck2019,
     title: 'awards',
     year: '2019',
     category: '2nd place	Innovative Chocolate	Spicy Mexican Hot Chocolate',
@@ -21,7 +24,7 @@ const award = [
       'In 2019, Cupcakes by Jenn debuted at the fair in over 10 competitions, and that first year was a disaster! Competing taught me valuable lessons. I learned to carefully track competition schedules—8 out of 10 days in 2019—and manage the challenges of baking daily while working my day job. Juggling up to 3 competitions some days, I realized they could be overwhelming. Additionally, I discovered the importance of closely following competition instructions; unfortunately, I was disqualified from 2 in 2019. Despite these hurdles, my first year was successful—I won 5 competitions.',
   },
   {
-    img: awardImg,
+    img: awardPic,
     title: 'awards',
     year: '2020',
     category: '',
@@ -30,7 +33,7 @@ const award = [
       'In 2020, the world came to a halt, including the state fair, providing an extra year for planning. The competition list is released in May, and registration is open until July 1st. Fortunately, you only need to specify the number of competitions, not their names. Entry fees cover all selected competitions. The list contains crucial details like judging dates, rules, criteria, and prizes for over 125 competitions with 600+ categories. While only a few are dessert-eligible, my creative approach helps me identify 15-20 appealing options, making the selection process more manageable.',
   },
   {
-    img: awardImg,
+    img: awardPic,
     title: 'awards',
     year: '2021',
     category: '',
@@ -39,7 +42,7 @@ const award = [
       'In 2021, the competition intensified as it appeared that everyone had picked up baking or cooking skills over the previous year. Despite entering 10 competitions, I only secured a placement in one, and the prize did not even cover the entry costs. It was a hit to my confidence after a successful debut year. However, I resolved not to give up easily and began planning for the next year.',
   },
   {
-    img: awardImg,
+    img: cupck2022,
     title: 'awards',
     year: '2022',
     category: '',
@@ -47,7 +50,7 @@ const award = [
     description: 'Information coming soon!',
   },
   {
-    img: awardImg,
+    img: cupck2023,
     title: 'awards',
     year: '2023',
     category: '',
